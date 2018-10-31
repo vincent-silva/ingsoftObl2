22df5e022a1169852c42624b647394028ad57726
vincent-silva <vincentsilva19@gmail.com>
2018-10-30 21:58:52 -0300

    Se aniaden metodos basicos a la clase mascota

--- Obligatorio2/src/sistema/Mascota.java
+++ Obligatorio2/src/sistema/Mascota.java
@@ -10,5 +10,55 @@ package sistema;
  * @author vince
  */
 public class Mascota {
-    
+    private String nomnbre;
+    private float altura;
+    private float peso;
+    private String comentarios;
+    private String imagen;
+
+    public Mascota(String nomnbre, float altura, float peso) {
+        this.nomnbre = nomnbre;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public String getNomnbre() {
+        return nomnbre;
+    }
+
+    public void setNomnbre(String nomnbre) {
+        this.nomnbre = nomnbre;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public String getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(String comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
 }
