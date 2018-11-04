4ff8877d9e22f95f0ddbc7086774d9158351f06b
vincent-silva <vincentsilva19@gmail.com>
2018-11-04 15:03:15 -0300

    Se agregan las clases para pruebas unitarias de sistema

--- Obligatorio2/test/pruebas/TestMascota.java
+++ Obligatorio2/test/pruebas/TestMascota.java
@@ -11,15 +11,14 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import modelo.*;
 
 /**
  *
  * @author vince
  */
-public class TestSistema {
+public class TestMascota {
     
-    public TestSistema() {
+    public TestMascota() {
     }
     
     @BeforeClass
