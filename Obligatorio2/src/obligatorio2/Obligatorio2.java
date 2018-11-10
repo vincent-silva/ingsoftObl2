ec926d1aad98b8eeccd49bd6c89cf3110a42d098
Vincent Silva <vincentsilva19@gmail.com>
2018-11-10 13:51:46 -0300

    Se agregan datos de prueba

--- Obligatorio2/src/obligatorio2/Obligatorio2.java
+++ Obligatorio2/src/obligatorio2/Obligatorio2.java
@@ -19,22 +19,18 @@ public class Obligatorio2 {
     public static void main(String[] args) {
         Sistema modelo = new Sistema();
         
-//        this.familia.agregarIntegrante(new Persona("Dniel Martinez", 55, 'M'));
-//        this.familia.agregarIntegrante(new Persona("Maria Marta Serra Lima", 77, 'F'));
-//        this.familia.agregarIntegrante(new Persona("Julieta Prandi", 40, 'F'));
-//        this.familia.agregarMascota(new Mascota("Ataque", 10, 10, 10, "Comentario", "una imagen"));
-//        this.familia.agregarMascota(new Mascota("Tony", 10, 10, 10, "otro comentario", "imagen"));
-        
         Persona integrante1 = new Persona("Barak Obama", 54, 'M');
         Persona integrante2 = new Persona("Mandrake Wolf", 29, 'M');
         Persona integrante3 = new Persona("Carla Peterson", 29, 'M');
+        Mascota mascota1 = new Mascota("Ataque", 10, 10, 10, "Comentario", "una imagen");
+        Mascota mascota2 = new Mascota("Tony", 10, 10, 10, "otro comentario", "imagen");
         System.out.println(integrante1);
         modelo.agregarIntegrante(integrante1);
+        modelo.agregarIntegrante(integrante2 );
+        modelo.agregarIntegrante(integrante3);
+        modelo.agregarMascota(mascota1);
+        modelo.agregarMascota(mascota2);
 
-//        
-//        Mascota mascota1 = new Mascota("Chicho", 10, 10, 10, "Comentario", "Foto");
-//        modelo.getFamilia().getMascotas().add(mascota1);
-        
 //        VentanaPrincipal ventana = new VentanaPrincipal(modelo);
 //        ventana.setVisible(true);
     }
