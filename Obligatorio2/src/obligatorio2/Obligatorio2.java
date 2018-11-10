f10626ad522cd4022bd11206e0c0650f0bff7134
vincent-silva <vincentsilva19@gmail.com>
2018-11-10 12:52:53 -0300

    Se crean datos de prueba en sistema

--- Obligatorio2/src/obligatorio2/Obligatorio2.java
+++ Obligatorio2/src/obligatorio2/Obligatorio2.java
@@ -5,6 +5,7 @@
  */
 package obligatorio2;
 
+import modelo.Mascota;
 import modelo.Persona;
 import modelo.Sistema;
 import vistas.VentanaPrincipal;
@@ -21,9 +22,13 @@ public class Obligatorio2 {
         Persona integrante1 = new Persona("Barak Obama", 54, 'M');
         Persona integrante2 = new Persona("Mandrake Wolf", 29, 'M');
         Persona integrante3 = new Persona("Carla Peterson", 29, 'M');
+        
         modelo.getFamilia().getIntegrantes().add(integrante1);
-        modelo.getFamilia().getIntegrantes().add(integrante2);
-        modelo.getFamilia().getIntegrantes().add(integrante3);
+//        modelo.getFamilia().getIntegrantes().add(integrante2);
+//        modelo.getFamilia().getIntegrantes().add(integrante3);
+//        
+//        Mascota mascota1 = new Mascota("Chicho", 10, 10, 10, "Comentario", "Foto");
+//        modelo.getFamilia().getMascotas().add(mascota1);
         
 //        VentanaPrincipal ventana = new VentanaPrincipal(modelo);
 //        ventana.setVisible(true);
