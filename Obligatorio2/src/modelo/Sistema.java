6e99085a2e8ebeb3fce103588f267e135ec1a02c
mjpla <mjpla@172.29.3.155>
2018-11-09 22:20:25 -0300

    Se agregan operaciones de sistemas

--- Obligatorio2/src/modelo/Sistema.java
+++ Obligatorio2/src/modelo/Sistema.java
@@ -5,12 +5,50 @@
  */
 package modelo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author vince
  */
 public class Sistema {
     
-    Familia familia = new Familia();
+    private Familia familia;
+    private ArrayList<Evento> eventos;
     
+    public Sistema(){
+        this.familia = null;
+        this.eventos = null;
+    }
+    public void setFamilia(Familia unaFamilia){
+        this.familia = unaFamilia;
+    }
+    public Familia getFamilia(){
+        return this.familia;
+    }
+    public void agregarEvento(Evento unEvento){
+        this.eventos.add(unEvento);
+    }
+    public void borrarEvento(Evento unEvento){
+        this.eventos.remove(unEvento);
+    }
+    public ArrayList<Evento> getEventos(){
+        return this.eventos;
+    }
+    public ArrayList<Paseo> getPaseos(){
+        ArrayList<Paseo> retorno = null;
+        for(Evento ev : this.eventos){
+            if(ev instanceof Paseo)
+                retorno.add((Paseo)ev);
+        }
+        return retorno;
+    }
+    public ArrayList<Comida> getComidas(){
+        ArrayList<Comida> retorno = null;
+        for(Evento com : this.eventos){
+            if(com instanceof Comida)
+                retorno.add((Comida)com);
+        }
+        return retorno;
+    }
 }
